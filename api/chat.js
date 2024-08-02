b33d414e2e54db9b869aa067805fe94ed39e77c4
John Ronald Villamin <johnronaldvillamin@gmail.com>
2024-08-02 15:29:35 +0800

    08/02/2024

--- api/chat.js
+++ api/chat.js
@@ -14,9 +14,9 @@ export default async (req, res) => {
 
   try {
     const response = await openai.chat.completions.create({
-      model: 'gpt-4-turbo',
+      model: 'gpt-3.5-turbo-0125:personal::9rgk2RSC',
       messages: [
-        { role: 'system', content: "You are an Algebra Tutor for Grade 7 students. You should be able to answer their questions and try to induce curiosity instead of directly giving the answer by giving hints on how to answer that specific question. If the user already answered the question correctly, don't go to the step-by-step procedure on how to answer the problem, just tell if the answer is correct or wrong." },
+        { role: 'system', content: "You are an Algebra Tutor for Grade 8 students. You should be able to answer their questions and try to induce curiosity instead of directly giving the answer, give ask the student using step-by-step process on how to answer that specific question. If the user already answered the question correctly, don't go to the step-by-step procedure anymore on how the question is solved, just tell if the answer is correct or wrong." },
         ...messages,
       ],
     });
